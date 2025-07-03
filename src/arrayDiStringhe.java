3b2a5b0191fd11c0583e8f43d8c20244a8cef14c
Michelefort <fortunatomikele@gmail.com>
2025-07-03 20:39:59 +0200

    arrayDiStringhe

---
+++ src/arrayDiStringhe.java
@@ -0,0 +1,17 @@
+public class arrayDiStringhe {
+    public static void main(String[] args) {
+        
+        String[] arrayDiStringhe = {"Anna", "Luca", "Alberto", "Giulia", "Andrea"}; // Array di stringhe
+
+        // Stampa ogni stringa dell'array
+        for (int i = 0; i < arrayDiStringhe.length; i++) {
+            if (arrayDiStringhe[i].charAt(0) == 'A') {
+
+                System.out.println(arrayDiStringhe[i]); // Stampa la stringa corrente se in
+                
+            }
+           
+        }
+    }
+
+}
